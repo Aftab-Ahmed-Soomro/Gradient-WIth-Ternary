64c6f825be131dda9206acedf9ca3331fef5d6e0
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2025-07-25 15:49:38 -0700

    added password toggle too

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react"
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 const App = () => {
 
   const [isVisible, setIsVisible] = useState(false)
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false)
 
   return (
     <div>
@@ -30,6 +33,28 @@ const App = () => {
       >
         {isVisible ? "✅ Visible: True" : "❌ Visible: False"}
       </p>
+    </div>
+    <div className="h-[50vh] flex flex-col justify-center items-center bg-blue-200 px-4">
+      <div className="w-full max-w-sm">
+        <h1 className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-transparent text-3xl mb-6
+      ">Password Visibility Toggle</h1>
+        <p className="mb-2 text-lg font-medium text-gray-700">Password</p>
+
+        <div className="relative">
+          <input
+            type={isPasswordVisible ? "text" : "password"}
+            placeholder="Enter your password"
+            className="w-full px-4 py-3 pr-12 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-green-500 transition"
+          />
+
+          <div
+            className="absolute inset-y-0 right-3 flex items-center text-gray-500 cursor-pointer"
+            onClick={() => setIsPasswordVisible((prev) => !prev)}
+          >
+            {isPasswordVisible ? <FaRegEyeSlash size={20} /> : <FaRegEye size={20} />}
+          </div>
+        </div>
+      </div>
     </div>
     </div>
   )
